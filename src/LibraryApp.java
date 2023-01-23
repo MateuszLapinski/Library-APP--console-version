a3d5625b53f66df37b794a9ed9501f6c6fca8b24
mateusz <mateuszlapinski.ml@gmail.com>
2023-01-23 22:10:44 +0100

    Przetłumaczone wersja

---
+++ src/LibraryApp.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LibraryApp {
+    private List<Book> books = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+
+    public void addBook() {
+        System.out.println("Entry the title:");
+        String title = scanner.nextLine();
+        System.out.println(" ");
+        System.out.println("Entry the author:");
+        String author = scanner.nextLine();
+        System.out.println(" ");
+        System.out.println("Is reading? (Y/N)");
+        String isReadString = scanner.nextLine();
+        System.out.println(" ");
+        boolean isRead;
+        if (isReadString.equals("Y")) {
+            isRead = true;
+            System.out.println("Book is read");
+        } else {
+            isRead = false;
+        }
+        Book book = new Book(title, author, isRead);
+        books.add(book);
+
+        System.out.println("Book has added");
+        System.out.println(" ");
+
+    }
+
+    public void displayBookByAuthor() {
+
+        System.out.println("Entry the author: ");
+        String authorUser = scanner.nextLine();
+        books.stream()
+                .filter(book -> book.getAuthor().equals(authorUser))
+                .forEach(Book::displayBookInformation);
+
+
+
+    }
+    public void displayIsReading() {
+        books.stream()
+                .filter(book -> book.getisRead()==false)
+                .forEach(Book::displayBookInformation);
+    }
+
+
+    public void displayBooks() {
+        int i=0;
+        for(Book  book: books) {
+            i++;
+            System.out.println(i+ ". " );
+            book.displayBookInformation();
+            System.out.println();
+
+
+        }
+        System.out.println(" ");
+    }
+    public void bookadding() {
+
+        Book book1= new Book("Ślepnąc od świateł","Żulczyk", true);
+        Book book2= new Book("Czarne Słońce","Żulczyk", true);
+        Book book3= new Book("Instytut","Żulczyk", false);
+        Book book4= new Book("Wzgórze Psów","Żulczyk", true);
+        Book book5= new Book("Harry Potter i Więzień Azkabanu","Rowling", true);
+        Book book6= new Book("Przystań Wiatrów","Martin", false);
+        Book book7= new Book("Krzyżacy","Sienkiewicz", true);
+        Book book8= new Book("Eragon","Paulini", true);
+        Book book9= new Book("Miecz przeznaczenia","Sapkowski", true);
+        Book book10= new Book("Krótka historia czasu", "Hawking", false);
+
+        books.add(book1);
+        books.add(book2);
+        books.add(book3);
+        books.add(book4);
+        books.add(book5);
+        books.add(book6);
+        books.add(book7);
+        books.add(book8);
+        books.add(book9);
+        books.add(book10);
+    }
+
+}
